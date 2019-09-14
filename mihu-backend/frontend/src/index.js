65e45d658d1fee9025d931854609d348a6f9ed62
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2019-09-14 10:41:58 +0530

    added backbone

--- mihu-backend/frontend/src/index.js
+++ mihu-backend/frontend/src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import AddItem from '../src/components/AddItem/Additem';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path='/' component={App}></Route>
+            <Route path='/add-item' component={AddItem}></Route>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+        </div>
+    </Router>, 
+document.getElementById('root'));
+
 serviceWorker.unregister();
