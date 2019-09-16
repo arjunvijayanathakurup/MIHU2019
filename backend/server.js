425043734e405cbe3f51bd8d385492876785ed9c
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2019-09-16 06:47:00 +0530

    added new attribute to mongodb connect

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,9 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
 
 connection.once('open', () => {
