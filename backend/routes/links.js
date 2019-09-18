7f3a9b1ac52e714591ceb0c6f36f263895035d4d
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2019-09-18 11:02:59 +0530

    added links backend

---
+++ backend/routes/links.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+let Link = require('../models/link.model');
+
+router.route('/').get((req, res) => {
+    Link.find()
+    .then(links => res.json(links))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const live = req.body.live;
+
+    const newLive = new Live({
+       live
+    });
+
+    newLive.save()
+    .then(() => res.json('New Live added!'))
+    .catch(err => res.status(400).json('Error ' + err));
+});
+
+module.exports = router;
