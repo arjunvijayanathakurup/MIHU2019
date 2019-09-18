da27da6da7664f132aa6154265dba2e44e618b96
Abhilash Saj <abhilashsaj@gmail.com>
2019-09-18 06:07:47 +0530

    Added Footer

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = (props) => {
+    
+    return (
+      <div style="text-align: center; padding-bottom: 10px; color: white;">Copyright 2019 CSA</div>
+    )
+
+}
+
+export default Footer
