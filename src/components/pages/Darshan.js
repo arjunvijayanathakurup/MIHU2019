97158123fafca80ec60748576c1b75b1debfe545
Abhilash Saj <abhilashsaj@gmail.com>
2019-09-19 06:16:19 +0530

    updated table

--- src/components/pages/Darshan.js
+++ src/components/pages/Darshan.js
@@ -1,64 +1,75 @@
 import React from 'react';
 import MaterialTable from 'material-table';
 
+// darshans{ DATE DARSHAN_TIME TOKEN_LOC TOKEN_TIME }
+
 export default function Darshan() {
   const [state, setState] = React.useState({
     columns: [
-      { title: 'Name', field: 'name' },
-      { title: 'Surname', field: 'surname' },
-      { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-      {
-        title: 'Birth Place',
-        field: 'birthCity',
-        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+      { title: 'Date', field: 'date', 
+      cellStyle: {
+        background: "inherit",
+        color: '#FFF'
       },
+      headerStyle: {
+        background: 'rgba(0, 0, 0, 0.6)',
+        color: 'white'
+      }
+    },
+      { title: 'Darshan TIme', field: 'darshan_time', 
+      cellStyle: {
+        background: "inherit",
+        color: '#FFF'
+      },
+      headerStyle: {
+        background: 'rgba(0, 0, 0, 0.6)',
+        color: 'white'
+      } },
+      { title: 'Token Location', field: 'token_loc', 
+      cellStyle: {
+        background: "inherit",
+        color: '#FFF'
+      },
+      headerStyle: {
+        background: 'rgba(0, 0, 0, 0.6)',
+        color: 'white'
+      }},
+      { title: 'Token Time', field: 'token_time' , 
+      cellStyle: {
+        background: 'inherit',
+        color: '#FFF'
+      },
+      headerStyle: {
+        background: 'rgba(0, 0, 0, 0.6)',
+        color: 'white'
+      }},
+      
+      
     ],
     data: [
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-      {
-        name: 'Zerya Betül',
-        surname: 'Baran',
-        birthYear: 2017,
-        birthCity: 34,
-      },
+      { date: '22/05/2019', darshan_time: '9:20 pm', token_loc: "entrance", token_time: '9:20 pm' },
+      { date: '22/05/2019', darshan_time: '9:20 pm', token_loc: "ashram", token_time: '9:20 pm' }
     ],
   });
-
+//   
   return (
-      <div className="container" > 
+      <div className="container" style={{border: "none"}}> 
         <MaterialTable
-        style={{background:" rgba(0, 0, 0, 0.6)", color: "white"}}
-            title="Editable Example"
+        style={{backgroundColor: "rgba(0, 0, 0, 0.6)", color: "white", borderRadius:"0px", border: "none"}}
+            
+            title="Darshan Timings"
             columns={state.columns}
             data={state.data}
-            editable={{
-                onRowAdd: newData =>
-                new Promise(resolve => {
-                    setTimeout(() => {
-                    resolve();
-                    const data = [...state.data];
-                    data.push(newData);
-                    setState({ ...state, data });
-                    }, 600);
-                }),
-                onRowUpdate: (newData, oldData) =>
-                new Promise(resolve => {
-                    setTimeout(() => {
-                    resolve();
-                    const data = [...state.data];
-                    data[data.indexOf(oldData)] = newData;
-                    setState({ ...state, data });
-                    }, 600);
-                }),
-                onRowDelete: oldData =>
-                new Promise(resolve => {
-                    setTimeout(() => {
-                    resolve();
-                    const data = [...state.data];
-                    data.splice(data.indexOf(oldData), 1);
-                    setState({ ...state, data });
-                    }, 600);
-                }),
+            options={{
+                headerStyle: {
+                    background: 'rgba(0, 0, 0, 0.6)',
+                    color: 'white'
+                },
+                searchFieldStyle: {
+                    background: 'inherit',
+                    color: 'white',
+                    padding: "5px"
+                },
             }}
             />
 
