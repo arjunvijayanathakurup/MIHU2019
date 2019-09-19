d95cb6956d0e3c99b97b74a6b35d8b16eeb6fa9f
Abhilash Saj <abhilashsaj@gmail.com>
2019-09-19 09:52:30 +0530

    added Food

---
+++ src/components/pages/Food.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import MaterialTable from 'material-table';
+
+// darshans{ DATE DARSHAN_TIME TOKEN_LOC TOKEN_TIME }
+
+export default function Food() {
+  const [state, setState] = React.useState({
+    columns: [
+      { title: 'Meal', field: 'meal', 
+      cellStyle: {
+        background: "inherit",
+        color: '#FFF'
+      },
+      headerStyle: {
+        background: 'rgba(0, 0, 0, 0.6)',
+        color: 'white'
+      }
+    },
+      { title: 'TIme', field: 'time', 
+      cellStyle: {
+        background: "inherit",
+        color: '#FFF'
+      },
+      headerStyle: {
+        background: 'rgba(0, 0, 0, 0.6)',
+        color: 'white'
+      } },
+      { title: 'Location', field: 'Location', 
+      cellStyle: {
+        background: "inherit",
+        color: '#FFF'
+      },
+      headerStyle: {
+        background: 'rgba(0, 0, 0, 0.6)',
+        color: 'white'
+      }},
+      { title: 'Counter', field: 'counter' , 
+      cellStyle: {
+        background: 'inherit',
+        color: '#FFF'
+      },
+      headerStyle: {
+        background: 'rgba(0, 0, 0, 0.6)',
+        color: 'white'
+      }},
+      
+      
+    ],
+    data: [
+      { date: '22/05/2019', darshan_time: '9:20 pm', token_loc: "entrance", token_time: '9:20 pm' },
+      { date: '22/05/2019', darshan_time: '9:20 pm', token_loc: "ashram", token_time: '9:20 pm' }
+    ],
+  });
+//   
+  return (
+      <div className="container" style={{border: "none"}}> 
+        <MaterialTable
+        style={{backgroundColor: "rgba(0, 0, 0, 0.6)", color: "white", borderRadius:"0px", border: "none"}}
+            
+            title="Food"
+            columns={state.columns}
+            data={state.data}
+            options={{
+                headerStyle: {
+                    background: 'rgba(0, 0, 0, 0.6)',
+                    color: 'white'
+                },
+                searchFieldStyle: {
+                    background: 'inherit',
+                    color: 'white',
+                    padding: "5px"
+                },
+            }}
+            />
+
+      </div>
+    
+  );
+}
