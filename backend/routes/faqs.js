09d3512cd5c639757f5cde5342db603c3b8b8c20
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2019-09-21 22:34:13 +0530

    added crud to faq details module

--- backend/routes/faqs.js
+++ backend/routes/faqs.js
@@ -21,4 +21,23 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error ' + err));
 });
 
+router.route('/update/:id').post((req, res) => {
+    Faq.findById(req.params.id)
+      .then(faqs => {
+        faqs.question = req.body.question;
+        faqs.answer = req.body.answer;
+
+        faqs.save()
+          .then(() => res.json('Faq details updated!'))
+          .catch(err => res.status(400).json('Error: ' + err));
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+  router.route('/:id').delete((req, res) => {
+    Faq.findByIdAndDelete(req.params.id)
+      .then(() => res.json('Faq details deleted.'))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
 module.exports = router;
