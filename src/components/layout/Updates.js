05f289c03f04df6647877969409ec64a4245eeba
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2019-09-25 12:30:10 +0530

    update

---
+++ src/components/layout/Updates.js
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+export default function Updates() {
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    axios.get('http://localhost:5000/live')
+    .then(json => setData(json.data))
+    .catch((error) => {
+      console.log(error);
+    })
+    
+  }, [])
+
+
+    return(
+      <>
+        <marquee behavior="scroll" direction="left">
+        {data.map((data) =><> 
+                {data.live}
+                <>&nbsp;</>
+                <>&nbsp;</>
+                <>&nbsp;</>
+                <>&nbsp;</>
+                </>
+                )}
+          </marquee> 
+      </>
+    )
+  }
